b16b48fdfb39b9c4d75cd614779616104c4a04f7
Khun Soe Yee <155002583+khunsoeyee@users.noreply.github.com>
2026-03-16 07:35:07 +0630

    Reorder assets array and ensure cache addition

--- sw.js
+++ sw.js
@@ -3,24 +3,24 @@ const assets = [
   './',
   './index.html',
   './page1.jpg',
-  './page2.jpg'
-  './page3.jpg'
-  './page4.jpg'
-  './page5.jpg'
-  './page6.jpg'
-  './page7.jpg'
-  './page8.jpg'
-  './page9.jpg'
-  './page10.jpg'
-  './page11.jpg'
-  './page12.jpg'
+  './page2.jpg',
+  './page3.jpg',
+  './page4.jpg',
+  './page5.jpg',
+  './page6.jpg',
+  './page7.jpg',
+  './page8.jpg',
+  './page9.jpg',
+  './page10.jpg',
+  './page11.jpg',
+  './page12.jpg',
   './page13.jpg'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
-      cache.addAll(assets);
+      return cache.addAll(assets);
     })
   );
 });
