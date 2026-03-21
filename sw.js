a7e2a4d5daff03078d4f2ad389468d839eb925d9
Khun Soe Yee <155002583+khunsoeyee@users.noreply.github.com>
2026-03-21 07:20:33 +0630

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,36 @@
-const CACHE_NAME = 'pao-book-v1';
-const urlsToCache = [
+const CACHE_NAME = 'book-cache-v1';
+const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './page1.jpg', './page2.jpg', './page3.jpg', './page4.jpg', './page5.jpg',
-  './page6.jpg', './page7.jpg', './page8.jpg', './page9.jpg', './page10.jpg',
-  './page11.jpg', './page12.jpg', './page13.jpg'
+  './page1.jpg',
+  './page2.jpg',
+  './page3.jpg',
+  './page4.jpg',
+  './page5.jpg',
+  './page6.jpg',
+  './page7.jpg',
+  './page8.jpg',
+  './page9.jpg',
+  './page10.jpg',
+  './page11.jpg',
+  './page12.jpg'
 ];
 
 // Install Service Worker
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-// Fetch resources from Cache
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+// Fetch Assets
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
